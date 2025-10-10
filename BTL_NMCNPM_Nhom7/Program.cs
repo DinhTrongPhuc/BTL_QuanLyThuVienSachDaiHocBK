73b4c706065c53342222b6a8ceb36819b73f1f99
DinhTrongPhuc <dnhtrongphuc210104@gmail.com>
2025-10-10 23:51:41 +0700

    Program.cs Commit

--- BTL_NMCNPM_Nhom7/Program.cs
+++ BTL_NMCNPM_Nhom7/Program.cs
@@ -3,16 +3,29 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Thêm DbContext
+// DbContext
 builder.Services.AddDbContext<LibraryContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("LibraryConnection")));
 
+// MVC
 builder.Services.AddControllersWithViews();
+
+// Session
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
 
 var app = builder.Build();
 
+// Pipeline
 app.UseStaticFiles();
 app.UseRouting();
+
+app.UseSession();      // <--- PHẢI có
 app.UseAuthorization();
 
 app.MapControllerRoute(
