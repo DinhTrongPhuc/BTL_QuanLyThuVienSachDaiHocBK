ebc995c24e71911cb014bf655f94dc3feb7e2db0
bùi trung <buivantrung682k4@gmail.com>
2025-10-11 17:51:26 +0700

    Cập nhật lt project

--- BTL_NMCNPM_Nhom7/Program.cs
+++ BTL_NMCNPM_Nhom7/Program.cs
@@ -3,20 +3,33 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Thêm DbContext
+// DbContext
 builder.Services.AddDbContext<LibraryContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("LibraryConnection")));
+	options.UseSqlServer(builder.Configuration.GetConnectionString("LibraryConnection")));
 
+// MVC
 builder.Services.AddControllersWithViews();
+
+// Session
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+	options.IdleTimeout = TimeSpan.FromMinutes(30);
+	options.Cookie.HttpOnly = true;
+	options.Cookie.IsEssential = true;
+});
 
 var app = builder.Build();
 
+// Pipeline
 app.UseStaticFiles();
 app.UseRouting();
+
+app.UseSession();      // <--- PHẢI có
 app.UseAuthorization();
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+	name: "default",
+	pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
