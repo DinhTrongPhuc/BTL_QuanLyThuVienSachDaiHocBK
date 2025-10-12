5ad0de378f689101c445cabe0fa6d5293c402ab2
bùi trung <buivantrung682k4@gmail.com>
2025-10-12 11:05:40 +0700

    vv

---
+++ BTL_NMCNPM_Nhom7/Program.cs
@@ -0,0 +1,35 @@
+using YourProject.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// DbContext
+builder.Services.AddDbContext<LibraryContext>(options =>
+	options.UseSqlServer(builder.Configuration.GetConnectionString("LibraryConnection")));
+
+// MVC
+builder.Services.AddControllersWithViews();
+
+// Session
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+	options.IdleTimeout = TimeSpan.FromMinutes(30);
+	options.Cookie.HttpOnly = true;
+	options.Cookie.IsEssential = true;
+});
+
+var app = builder.Build();
+
+// Pipeline
+app.UseStaticFiles();
+app.UseRouting();
+
+app.UseSession();      // <--- PHẢI có
+app.UseAuthorization();
+
+app.MapControllerRoute(
+	name: "default",
+	pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
